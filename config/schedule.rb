b2d39e66cbcfedad678c5981ab7016ae0c48ac94
NhanNT <nhannt.bk@gmail.com>
2017-12-17 21:39:22 +0700

    Add front-end

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
-# env :PATH, ENV['PATH']
-# set :environment, 'development'
-# set :output, "#{path}/log/cron.log"
-# every :minute do
-#   runner "DatabaseHandler.get_post"
-# end
+env :PATH, ENV['PATH']
+set :environment, 'development'
+set :output, "#{path}/log/cron.log"
+every :minute do
+  runner "DatabaseHandler.get_post"
+end
