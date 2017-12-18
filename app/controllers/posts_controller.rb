2d25d3e1531da0ab41c34cd72d415286ea35353f
THM1707 <tranhoangminh1707@gmail.com>
2017-12-18 22:54:49 +0700

    Fix remaining bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
       @posts.keep_if { |p| p.message.present? && p.message.include?(params[:filter]) }
     end
     if params[:shop].present?
-      @page = Page.find_by_name!(params[:shop])
+      @page = Page.find_by!(permalink_url: params[:shop])
       @posts.keep_if { |p| p.message.present? && p.page_id == @page.id }
     end
     if params[:category].present?
@@ -15,8 +15,8 @@ class PostsController < ApplicationController
       @keywords = @category.keywords.split(',')
       @posts.keep_if { |p| p.message.present? && p.in_category?(@keywords) }
     end
-    if params[:page].present? then
-      if params[:limit].present? then
+    if params[:page].present?
+      if params[:limit].present?
         @posts = @posts.paginate(:page => params[:page], :per_page =>  params[:limit])
       else
         @posts = @posts.paginate(:page => params[:page], :per_page => 12)
